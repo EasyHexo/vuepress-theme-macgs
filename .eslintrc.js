d911b469b480b6623ceeb232c75e9f3a985aa874
Mitscherlich <mitscherlich36@gmail.com>
2019-02-11 17:20:31 +0800

    :sparkles: Update theme macgs to 1.0.0-alpha-37

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['standard', 'plugin:vue/essential', 'plugin:prettier/recommended'],
+  plugins: ['node', 'promise'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
